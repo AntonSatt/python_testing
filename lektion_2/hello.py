f8e21cd39e2813c447067388365806eba442655b
Anton Satt <makeorbreak+github@pm.me>
2025-09-22 15:19:58 +0200

    exercise

--- lektion_2/hello.py
+++ lektion_2/hello.py
@@ -5,6 +5,5 @@ name = input("What is your name? ")
 first_number = int(input("First number? "))
 second_number = int(input("Second number? "))
 
-sum = first_number + second_number
 
-print(f"Hi {name}! The total of your numbers are {sum}. ^_^")
+print(f"Hi {name}! The total of your numbers are {first_number + second_number}. ^_^")
