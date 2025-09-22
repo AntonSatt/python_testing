346174007968ca4779c7efbe83863ba247152177
Anton Satt <makeorbreak+github@pm.me>
2025-09-22 15:52:00 +0200

    loops

---
+++ lektion_2/loops.py
@@ -0,0 +1,18 @@
+import sys
+
+app_is_running = True
+
+while app_is_running == True:
+    print("Menu 1")
+    print("Menu 2")
+
+    menu_choice = input("Vilken menu? ")
+
+    if (menu_choice == "1"):
+        print("Menu 1 vald!")
+    elif (menu_choice == "2"):
+        print("Menu 2 vald!")
+    else:
+        print("Inkorrekt val!")
+
+    sys.exit()    
