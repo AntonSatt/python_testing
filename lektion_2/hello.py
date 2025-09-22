9ecfa1184c99c1a62b8d850f8a416becc379a926
Anton Satt <makeorbreak+github@pm.me>
2025-09-22 14:44:21 +0200

    Hello DevOps-script

---
+++ lektion_2/hello.py
@@ -0,0 +1,10 @@
+#Hello DevOps-script 
+
+name = input("What is your name? ")
+
+first_number = int(input("First number? "))
+second_number = int(input("Second number? "))
+
+sum = first_number + second_number
+
+print(f"Hi {name}! The total of your numbers are {sum}. ^_^")
